37250d5ff011bb13664fd6bef877bbb382012819
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-03-31 10:14:09 +0200

    initial tasks

---
+++ Test.java
@@ -0,0 +1,31 @@
+package de;
+/**
+   change all public mehtods named test* to protected
+
+   change word: cw
+   search: /
+   next match: n
+   repeat: .
+*/
+class Test {
+
+  public static String test() {
+    return null;
+  }
+
+  public static String test2() {
+    return null;
+  }
+
+  public static int moeafdp(int publications) {
+    return publications + 1;
+  }
+
+  public static String test1() {
+    return null;
+  }
+
+  public static String moep() {
+    return null;
+  }
+}
